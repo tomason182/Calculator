6fca380e513063baa5ebe687ace3104e94df62ec
tomason182 <elotro182@hotmail.com>
2024-02-24 15:43:34 -0300

    show result in display. need more logic

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let listOfWanders = [];
+let result = 0;
 
 const display = document.querySelector('#display');
 
@@ -13,7 +14,8 @@ pressEspecial.forEach((button) => {
     button.addEventListener('click', () => displayEspecial(button));
 })
 
-let pressEquals = document.querySelector('.equals')
+let pressEquals = document.querySelector('.equals');
+pressEquals.addEventListener('click', () => makeCalculus());
 
 // functions section
 
@@ -28,10 +30,11 @@ function displayNum(a) {
 
     display.appendChild(content);
 
-    console.log(listOfWanders);
+    // console.log(listOfWanders);
 }
 
 function displayEspecial (b) {
+
     let pressedEspecial = b.textContent;    
 
     if (listOfWanders.length === 0 || isNaN(parseFloat(listOfWanders[listOfWanders.length -1]))) {
@@ -41,16 +44,61 @@ function displayEspecial (b) {
     }else{
 
         listOfWanders.push(pressedEspecial);
+
         const content = document.createElement('div');
         content.classList.toggle('content');
         content.textContent = pressedEspecial;
-        content.setAttribute('style', 'font-size: 15px;');
+        content.setAttribute('style', 'font-size: 25px;');
     
         display.appendChild(content);
     };
 
 }
-   
 
+function makeCalculus() {
 
-console.log(listOfWanders.length)
+    let numOne;
+    let numTwo;
+    let operator;
+    let indexOperator;
+    const operators = ["+", "-", "x", "÷"];
+
+    for (let i=0; i < operators.length; i++) {
+
+        indexOperator = listOfWanders.findIndex((j) => j === operators[i]);
+
+        if (indexOperator === -1 ) {
+            continue;            
+        }else{
+            operator = operators[i];
+            numOne = parseFloat(listOfWanders.slice(0, indexOperator).join(""));
+            numTwo = parseFloat(listOfWanders.slice(indexOperator + 1).join(""));
+            
+            if (operator === '+'){
+                result = numOne + numTwo;
+            }else if(operator === '-'){
+                result = numOne - numTwo;
+            }else if (operator === 'x'){
+                result = numOne * numTwo;
+            }else{
+                result = numOne / numTwo;
+            };
+        }
+    }
+
+    if (operator === undefined){
+        result = parseFloat(listOfWanders.join(""));
+    }
+
+    const remuveOperation = document.querySelectorAll('.content');
+    remuveOperation.forEach((item) => item.remove());
+
+    const content = document.createElement('div');
+    content.classList.toggle('content');
+    content.textContent = result;
+    content.setAttribute('style', 'font-size: 25px;');
+
+    display.appendChild(content);
+
+    
+}

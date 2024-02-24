1e570e14b3e263dae0f838bae7280ab588a115cc
tomason182 <elotro182@hotmail.com>
2024-02-24 09:57:37 -0300

    show numbers in display

--- script.js
+++ script.js
@@ -2,18 +2,7 @@ let listOfWanders = [];
 
 const display = document.querySelector('#display');
 
-const content = document.createElement('p');
-content.classList.toggle('content');
-
-
-
-
-
-
 let pressNum = document.querySelectorAll('button');
-
-
-
 
 pressNum.forEach((button) => {
     button.addEventListener('click', () => calculator(button));
@@ -24,31 +13,11 @@ function calculator(a) {
     let pressedButton = a.textContent;
     listOfWanders.push(pressedButton);
     
-    firstItem = parseFloat(listOfWanders[0]);
-    if (isNaN(firstItem)) {
-        listOfWanders.shift();
-    };
-    
-    for (i = 0; listOfWanders.length -1; i++){
-    
-        content.textContent = listOfWanders[i];
-        content.setAttribute('style', 'font-size: 25px; text-align: right; padding-right: 8px');
+    const content = document.createElement('div');
+    content.classList.toggle('content');
+    content.textContent = pressedButton;
+    content.setAttribute('style', 'font-size: 25px; text-align: right; padding-right: 8px; display: inline-block');
 
-        display.appendChild(content);
-    }
-    
-
-    // aqui tendria que irse mostrando los numero impresos en display
-  
-
-
-   /*  for (i = 0; listOfWanders.length -1; i++) {
-        
-    }
-
-
-
-    return console.log(listOfWanders); */
-
+    display.appendChild(content);
 
 }

4e21c962497566971540127dd45fc1330f82f837
tomason182 <elotro182@hotmail.com>
2024-02-24 11:55:29 -0300

    change display functin

--- script.js
+++ script.js
@@ -2,22 +2,26 @@ let listOfWanders = [];
 
 const display = document.querySelector('#display');
 
-let pressNum = document.querySelectorAll('button');
+let pressNum = document.querySelectorAll('.number');
 
 pressNum.forEach((button) => {
-    button.addEventListener('click', () => calculator(button));
+    button.addEventListener('click', () => displayNum(button));
 });
 
+let pressEspecial = 
 
-function calculator(a) {
+
+function displayNum(a) {
     let pressedButton = a.textContent;
     listOfWanders.push(pressedButton);
     
     const content = document.createElement('div');
     content.classList.toggle('content');
     content.textContent = pressedButton;
-    content.setAttribute('style', 'font-size: 25px; text-align: right; padding-right: 8px; display: ');
+    content.setAttribute('style', 'font-size: 25px; flex-shrink: ');
 
     display.appendChild(content);
+
+    console.log(listOfWanders)
 
 }
